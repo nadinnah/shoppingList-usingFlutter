48ea5e81f5cd88484c1042569b4f4338df92fe7f
nadinnah <nadinahmedmohamed@gmail.com>
2026-05-25 19:33:28 +0300

    add initial values in the form

--- lib/screens/new_item.dart
+++ lib/screens/new_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../data/categories.dart';
+import '../models/category.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -10,81 +11,115 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItem> {
-  final _formKey= GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
+  var _enteredName = '';
+  var _enteredQuantity = 1;
+  var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem(){
-    _formKey.currentState!.validate();
+  void _saveItem() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+    }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Add new Item'),
-      ),
+      appBar: AppBar(title: Text('Add new Item')),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
         child: Form(
-          key:_formKey,
-          child: Column(children: [
-            TextFormField(
-              maxLength: 50,
-              decoration: InputDecoration(
-                label: Text('Name'),
-
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: InputDecoration(label: Text('Name')),
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'Name cannot be empty';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _enteredName = value!;
+                },
               ),
-              validator: (value){
-                if(value==null || value.trim().isEmpty){
-                  return 'Name cannot be empty';
-                }
-                return null;
-              },
-            ),
-            Row(crossAxisAlignment: CrossAxisAlignment.end  //CrossAxisAlignment.end is used to align the children to the end of the row vertically
-                ,children: [
-              Expanded(
-                child: TextFormField( //wrap TextFormField with expanded as it's unconstrained horizontally like row
-                  decoration: InputDecoration(
-                    label: Text('Quantity'),
-                  ),
-                  initialValue: '1',
-                  keyboardType: TextInputType.number,
-                  validator: (value){
-                    if(value==null || value.isEmpty || int.tryParse(value)==null || int.parse(value)<=0){
-                      return 'Must be a valid positive number';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              SizedBox(width: 8,),
-              Expanded(
-                child: DropdownButtonFormField(items: [for(final category in categories.entries) //entries converts the map into a list of entries
-                  DropdownMenuItem(value: category.value, child: Row(children: [
-                    Container(
-                      width: 16,
-                      height: 16,
-                      color: category.value.color,
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                //CrossAxisAlignment.end is used to align the children to the end of the row vertically
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      //wrap TextFormField with expanded as it's unconstrained horizontally like row
+                      decoration: InputDecoration(label: Text('Quantity')),
+                      initialValue: _enteredQuantity.toString(),
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.parse(value) <= 0) {
+                          return 'Must be a valid positive number';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _enteredQuantity = int.parse(
+                          value!,
+                        ); //parse throws error if value is null, tryParse returns null if value is null
+                      },
                     ),
-                    SizedBox(width: 6,),
-                    Text(category.value.title),
-                  ],))], onChanged:
-                (value){
-
-                }),
-              )
-            ],),
-            SizedBox(height: 16,),
-            Row(mainAxisAlignment: MainAxisAlignment.end,children: [
-              TextButton(onPressed: (){
-                _formKey.currentState!.reset();
-              }, child: Text('Reset')),
-              ElevatedButton(onPressed: _saveItem, child: Text('Add Item')),
-            ],)
-
-          ])
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      initialValue: _selectedCategory,
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedCategory = value!;
+                        });
+                      },
+                      items: [
+                        for (final category
+                            in categories
+                                .entries) //entries converts the map into a list of entries
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                SizedBox(width: 6),
+                                Text(category.value.title),
+                              ],
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      _formKey.currentState!.reset();
+                    },
+                    child: Text('Reset'),
+                  ),
+                  ElevatedButton(onPressed: _saveItem, child: Text('Add Item')),
+                ],
+              ),
+            ],
+          ),
         ),
-      )
-
+      ),
     );
   }
 }
